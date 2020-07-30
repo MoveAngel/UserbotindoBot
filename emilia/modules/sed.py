1952377c4e3caefac275ee7bc732c120ae5364ca
Tomy Prasetio <58870823+tomyprs25@users.noreply.github.com>
2020-07-30 16:16:30 +0700

    Updated, but still bot tested (#4)
    * Locks: Add inline lock support since telegram added 'via_bot' field in Message class, also add PEP8 linting in locks module.
    
    Add these column in database=>
    
    alter table permissions add column inline bool default false;
    
    Signed-off-by: starry69 <starry369126@outlook.com>
    
    * fix one formatting issue
    
    Signed-off-by: starry69 <starry369126@outlook.com>
    
    * sed.py: Fixed Regex Handler
    
    Signed-off-by: Tomi <tomy.prs25@gmail.com>
    
    * Fixed verifications and other things
    
    - Fixed emoji verifications, bot will send a new file instead of cache, to prevent bot send same photo again and again
    - Fixed promote, an admin should have can_promote_members permission to promote someone
    - Fixed 'restricted' list, bot will give member full permission after verified
    
    * Fixed backup for fitlers text and buttons
    
    Co-authored-by: starry69 <starry369126@outlook.com>
    Co-authored-by: Ayra Hikari <ayrahikari@linuxmail.org>
    Co-authored-by: Ayra Hikari <AyraHikari@gmail.com>

--- emilia/modules/sed.py
+++ emilia/modules/sed.py
@@ -6,8 +6,8 @@ import telegram
 from telegram import Update, Bot
 from telegram.ext import run_async
 
-from emilia import dispatcher, LOGGER
-#from emilia.modules.disable import DisableAbleRegexHandler
+from emilia import dispatcher, LOGGER, spamcheck
+from emilia.modules.disable import DisableAbleRegexHandler
 
 DELIMITERS = ("/", ":", "|", "_")
 
@@ -59,11 +59,9 @@ start_time = time.time()
 max_time = 5
 
 @run_async
+@spamcheck
 def sed(bot: Bot, update: Update):
     start = time.time()
-    spam = spamfilters(update.effective_message.text, update.effective_message.from_user.id, update.effective_chat.id, update.effective_message)
-    if spam == True:
-        return
     if update.effective_message.from_user.id != 388576209:
         return
     while elapsed_time() < max_time:
@@ -129,6 +127,6 @@ seperti: \\?.
 __mod_name__ = "Sed/Regex"
 
 
-#SED_HANDLER = DisableAbleRegexHandler(r's([{}]).*?\1.*'.format("".join(DELIMITERS)), sed, friendly="sed")
+SED_HANDLER = DisableAbleRegexHandler(r's([{}]).*?\1.*'.format("".join(DELIMITERS)), sed, friendly="sed")
 
-#dispatcher.add_handler(SED_HANDLER)
+dispatcher.add_handler(SED_HANDLER)
