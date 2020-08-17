7c1d6edb0cd0c6edc9f4328a08f9b14d4861d8ee
MoveAngel <moveangel@minaproject.tech>
2020-08-17 12:48:18 +0700

    global_bans: fix typo on support group
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- userindobot/modules/global_bans.py
+++ userindobot/modules/global_bans.py
@@ -222,7 +222,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
         bot.send_message(
             user_id,
             "You have been globally banned from all groups where I have administrative permissions."
-            "If you think that this was a mistake, you may appeal your ban here: @userindobotEagleSupport",
+            "If you think that this was a mistake, you may appeal your ban here: @userindobot",
             parse_mode=ParseMode.HTML)
     except Exception as e:
         print(e)
